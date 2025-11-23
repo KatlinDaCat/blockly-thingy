b4eccae967c5efc24fc9c74a4ebc8453dd2b763f
Katlin <Kaden1084@gmail.com>
2025-11-23 14:13:58 -0600

    stuff

--- src/blocks.js
+++ src/blocks.js
@@ -1,6 +1,24 @@
 Blockly.common.defineBlocksWithJsonArray([
     {
         "type": "main",
-        "message0": ""
+        "message0": "main ",
+        "message1": "%1",
+        "args1": [
+            {
+                "type": "input_statement",
+                "name": "FUNC"
+            }
+        ]
+    },
+    {
+        "type": "functions_define",
+        "message0": "function ",
+        "message1": "%1",
+        "args1": [
+            {
+                "type": "input_statement",
+                "name": "FUNC"
+            }
+        ]
     }
-])
+]);
